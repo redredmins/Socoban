cf9bc2d02e329cdcee07b417798427760117bf58
redredmins <redminsgame@gmail.com>
2020-09-09 18:17:59 +0900

    2

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // 
+    char[][] stage = new char[10][]
+    {
+        //              0    1    2    3    4    5    6    7    8    9
+        new char[10] { 'W', 'W', 'W', 'W', 'W', '.', 'W', 'W', 'W', '.'}, // 0
+        new char[10] { 'W', '.', '.', '.', 'W', '.', 'W', 'G', 'W', '.'}, // 1
+        new char[10] { 'W', '.', 'I', '.', 'W', '.', 'W', 'G', 'W', '.'}, // 2
+        new char[10] { 'W', '.', 'I', '.', 'W', '.', 'W', 'G', 'W', '.'}, // 3
+        new char[10] { 'W', 'W', 'W', '.', 'W', 'W', 'W', '.', 'W', '.'}, // 4
+        new char[10] { '.', 'W', 'W', '.', '.', 'P', '.', '.', 'W', '.'}, // 5
+        new char[10] { '.', 'W', '.', 'I', '.', 'W', '.', '.', 'W', '.'}, // 6
+        new char[10] { '.', 'W', '.', '.', '.', 'W', 'W', 'W', 'W', '.'}, // 7
+        new char[10] { '.', 'W', 'W', 'W', 'W', 'W', '.', '.', '.', '.'}, // 8
+        new char[10] { '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'}  // 9
+    };
+
+    public Transform stageTransform;
+    public GameObject wallPrefab;
+
+
+    void Start()
+    {
+        for(int z = 0; z < stage.Length; ++z)
+        {
+            for(int x = 0; x < stage[z].Length; ++x)
+            {
+                if (stage[z][x] == 'W')
+                {
+                    GameObject wallObj = Instantiate(wallPrefab, stageTransform);
+
+                    Vector3 newPos = new Vector3(x, 0.5f, -z);
+                    Vector3 correction = new Vector3((stage[z].Length * 0.5f), 0f, -(stage.Length * 0.5f));
+                    wallObj.transform.localPosition = newPos - correction - new Vector3(-0.5f, 0f, 0.5f);
+                }
+            }
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/ItemBox.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBox : MonoBehaviour
+{
+    //public Collider[] colliders;
+    public Color touchColor;
+    Color originColor;
+
+    Renderer renderer;
+
+
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        originColor = renderer.material.color;
+    }
+    
+    void OnTriggerEnter(Collider collider)
+    {
+        //Debug.Log(collider.gameObject.name + " 이 부딪힘");
+
+        SetTouchColor(collider.tag);
+    }
+
+    void OnTriggerStay(Collider collider)
+    {
+        SetTouchColor(collider.tag);
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.tag == "Goal")
+        {
+            renderer.material.color = originColor;
+        }
+    }
+
+    void SetTouchColor(string tag)
+    {
+        if (tag == "Goal")
+        {
+            renderer.material.color = touchColor;
+        }
+    }
+
+    void CellMove(Vector3 colliderPos)
+    {
+        Vector3 myPos = transform.position;
+        float xDist = colliderPos.x - myPos.x;
+        float zDist = colliderPos.z - myPos.z;
+        if (xDist > zDist)
+        {
+            if (myPos.x > colliderPos.x) // right
+            {
+
+            }
+            else // left
+            {
+
+            }
+        }
+        else
+        {
+            if (myPos.z > colliderPos.z) // up
+            {
+
+            }
+            else
+            {
+
+            }
+
+        }
+    }
+}
