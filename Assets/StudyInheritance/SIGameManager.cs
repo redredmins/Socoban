5f8410b291399d6b7f3cbece881def365a4b1bc9
redredmins <redminsgame@gmail.com>
2020-09-11 17:56:56 +0900

    상속 숙제 답..

--- Assets/StudyInheritance/SIGameManager.cs
+++ Assets/StudyInheritance/SIGameManager.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class SIGameManager : MonoBehaviour
 {
+    /*
+    1. 추상 클래스 Mover 를 만드시고,
+       추상 메서드 void Move(), void Stop() 선언.
+    
+    2. Mover를 상속받은 Car, Airplane 클래스를 만드시고,
+       자동차는 바닥을 x, z축으로 움직이고,
+       비행기는 바닥에서 하늘로(y축)으로 뜨고 착륙하길 반복
+       다른 움직임을 하도록 구현.
+    
+    3. 자동차, 비행기 프리팹 만들어서
+       게임 메니저가 랜덤한 위치에 각각 3개, 1개 생성.
+    
+    4. UI버튼 만들고, 버튼을 누르면 모든 Mover가 멈추도록.
+    */
+
     public GameObject carPrefab;
     public GameObject airplanePrefab;
 
